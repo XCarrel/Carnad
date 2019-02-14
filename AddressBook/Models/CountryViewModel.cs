b650f9c1aa542bc6f5c0cfbd3d770da9189d6086
Xavier Carrel <xavier.carrel@cpnv.ch>
2019-02-14 11:59:34 +0100

    Use data annotation property
    On Country.Dialcode

--- AddressBook/Models/CountryViewModel.cs
+++ AddressBook/Models/CountryViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace AddressBook.Models
     public class Country
     {
         public string Name { get; set; }
+        [DisplayName("+")]
         public int DialCode { get; set; }
         public Country(string name, int dialCode)
         {
