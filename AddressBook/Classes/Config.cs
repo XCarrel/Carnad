8a3fddafa74b025ca308dec936b91b032f0154fd
MisterX25 <Xavier.Carrel@cpnv.ch>
2019-02-20 13:54:05 +0100

    Show a version number in the layout

---
+++ AddressBook/Classes/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class AppConfig
+    {
+        public static string sVersion = "1.0";
+    }
+}
