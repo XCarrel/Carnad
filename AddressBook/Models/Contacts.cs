bc10bf73a5144ad1e19f316e1d4e0af9d3accfdc
MisterX25 <Xavier.Carrel@cpnv.ch>
2019-03-03 08:41:36 +0100

    Regenerated model

--- AddressBook/Models/Contacts.cs
+++ AddressBook/Models/Contacts.cs
@@ -15,6 +15,8 @@ namespace AddressBook.Models
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
+        public byte BlackList { get; set; }
+        public byte? Gender { get; set; }
         public int CountryId { get; set; }
 
         public Countries Country { get; set; }
