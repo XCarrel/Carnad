a42efed0141015f0638d7abd85992717dee04aba
MisterX25 <Xavier.Carrel@cpnv.ch>
2019-03-03 15:51:45 +0100

    Use metadata for user display

---
+++ AddressBook/Models/PartialClasses/Contacts.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Models
+{
+    [ModelMetadataType(typeof(ContactsMetadata))]
+    public partial class Contacts
+    {
+        [NotMapped]
+        [Display(Name = "Liste noire")]
+        public bool BoolBlackList
+        {
+            get { return BlackList != 0; }
+            set { BlackList = (byte)(value ? 1 : 0); }
+        }
+    }
+
+    public class ContactsMetadata
+    {
+        [Display(Name = "Prénom")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Nom")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Téléphone")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Courriel")]
+        public string Email { get; set; }
+
+        [Display(Name = "Genre")]
+        public byte? Gender { get; set; }
+
+        [Display(Name = "Pays")]
+        public Countries Country { get; set; }
+
+        [Display(Name = "Pays")]
+        public int CountryId { get; set; }
+    }
+
+}
---
+++ AddressBook/Models/PartialClasses/Countries.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Models
+{
+    [ModelMetadataType(typeof(CountriesMetadata))]
+    public partial class Countries
+    {
+    }
+
+    public class CountriesMetadata
+    {
+        [Display(Name = "Indicatif international")]
+        public int? Dialcode { get; set; }
+
+        [Display(Name = "Pays")]
+        public string Name { get; set; }
+    }
+}
