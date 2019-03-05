4a5f07a971768cfb1ffea7f92a74e8675fbb709f
MisterX25 <Xavier.Carrel@cpnv.ch>
2019-03-05 10:06:21 +0100

    Have a basic API
    Generated by VS (scaffolded item)

---
+++ AddressBook/Controllers/GroupsController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AddressBook.Models;
+
+namespace AddressBook.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupsController : ControllerBase
+    {
+        private readonly CarnadContext _context;
+
+        public GroupsController(CarnadContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Groups
+        [HttpGet]
+        public IEnumerable<Groups> GetGroups()
+        {
+            return _context.Groups;
+        }
+
+        // GET: api/Groups/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetGroups([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var groups = await _context.Groups.FindAsync(id);
+
+            if (groups == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(groups);
+        }
+
+        // PUT: api/Groups/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGroups([FromRoute] int id, [FromBody] Groups groups)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != groups.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(groups).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GroupsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Groups
+        [HttpPost]
+        public async Task<IActionResult> PostGroups([FromBody] Groups groups)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Groups.Add(groups);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGroups", new { id = groups.Id }, groups);
+        }
+
+        // DELETE: api/Groups/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGroups([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var groups = await _context.Groups.FindAsync(id);
+            if (groups == null)
+            {
+                return NotFound();
+            }
+
+            _context.Groups.Remove(groups);
+            await _context.SaveChangesAsync();
+
+            return Ok(groups);
+        }
+
+        private bool GroupsExists(int id)
+        {
+            return _context.Groups.Any(e => e.Id == id);
+        }
+    }
+}
