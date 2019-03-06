6b7a0cd12f48b5458cbf10a77210137c579142af
MisterX25 <Xavier.Carrel@cpnv.ch>
2019-03-06 14:49:37 +0100

    Use the end point to validate group name

--- AddressBook/js/carnad.js
+++ AddressBook/js/carnad.js
@@ -2,4 +2,18 @@
     $('.clickable-row').click(function () {
         window.location = $(this).data('href')
     })
+
+    // Group name validation
+    $('#newgroup').keyup(function () {
+        $('#cmdNew').prop('disabled', $(this).val().length < 2) // group name must be at least 2 chars long
+        $.get("api/Groups/Exists/" + $(this).val(), function (data, status) {
+            if (data) { // group name already exists
+                $('#cmdNew').prop('disabled', true)
+                $('#errgrp').removeClass('hidden')
+            }
+            else {
+                $('#errgrp').addClass('hidden')
+            }
+        });
+    })
 })
